e060a6fdf67d13b6051e2dfb619348e52b6bd156
mad1ost <mad1ost@mail.ru>
2020-07-21 21:14:31 +0300

    do not work on blocked domains https://developer.mozilla.org/docs/Mozilla/Add-ons/WebExtensions/Content_scripts

--- background.js
+++ background.js
@@ -1,6 +1,24 @@
 'use strict';
 
 const tabs = {};
+// https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Content_scripts
+const blockedDomains = [
+  'accounts-static.cdn.mozilla.net',
+  'accounts.firefox.com',
+  'addons.cdn.mozilla.net',
+  'addons.mozilla.org',
+  'api.accounts.firefox.com',
+  'content.cdn.mozilla.net',
+  'content.cdn.mozilla.net',
+  'discovery.addons.mozilla.org',
+  'input.mozilla.org',
+  'install.mozilla.org',
+  'oauth.accounts.firefox.com',
+  'profile.accounts.firefox.com',
+  'support.mozilla.org',
+  'sync.services.mozilla.com',
+  'testpilot.firefox.com'
+];
 
 function changeIcon(tabId, details, event) {
   if (!(tabId in tabs)) {
@@ -44,6 +62,8 @@ browser.webRequest.onResponseStarted.addListener(
 browser.webNavigation.onCommitted.addListener(
   (details) => {
     if (details.frameId !== 0) return;
+    const url = new URL(details.url);
+    if (blockedDomains.indexOf(url.hostname) !== -1) return;
     changeIcon(details.tabId, details, 'onCommitted');
   },
   {
