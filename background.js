b86e3183797e52febd1bb1d3a573f0a3866766fc
mad1ost <mad1ost@mail.ru>
2023-03-27 02:48:03 +0300

    refactoring

--- background.js
+++ background.js
@@ -5,54 +5,27 @@ const tabs = {};
 function updateIcon(details, event) {
 	const tabId = details.tabId;
 	if (!(tabId in tabs)) {
-		tabs[tabId] = {
-			onResponseStarted: null,
-			onCommitted: null
-		}
+		tabs[tabId] = { onResponseStarted: null, onCommitted: null };
 	}
 	const tab = tabs[tabId];
 	tab[event] = details;
 	if (tab.onCommitted && tab.onResponseStarted === null) {
-		if (details.transitionQualifiers.indexOf('forward_back') !== -1) { // bfcache
-			browser.pageAction.setIcon({
-				tabId: details.tabId,
-				path: 'icons/no-ip.svg'
-			});
-			browser.pageAction.setTitle({
-				tabId: details.tabId,
-				title: browser.i18n.getMessage('from_cache')
-			});
+		if (tab.onCommitted.transitionQualifiers.indexOf('forward_back') !== -1) { // bfcache
+			browser.pageAction.setIcon({ tabId: tabId, path: 'icons/no-ip.svg' });
+			browser.pageAction.setTitle({ tabId: tabId, title: browser.i18n.getMessage('from_cache') });
 		}
 	} else if (tab.onCommitted && tab.onResponseStarted) {
 		if (tab.onResponseStarted.timeStamp >= tab.onCommitted.timeStamp) return;
 		if (tab.onResponseStarted.fromCache) {
-			browser.pageAction.setIcon({
-				tabId: tabId,
-				path: 'icons/no-ip.svg'
-			});
-			browser.pageAction.setTitle({
-				tabId: tabId,
-				title: browser.i18n.getMessage('from_cache')
-			});
+			browser.pageAction.setIcon({ tabId: tabId, path: 'icons/no-ip.svg' });
+			browser.pageAction.setTitle({ tabId: tabId, title: browser.i18n.getMessage('from_cache') });
 		} else if (tab.onResponseStarted.proxyInfo !== null) {
-			browser.pageAction.setIcon({
-				tabId: tabId,
-				path: 'icons/no-ip.svg'
-			});
-			browser.pageAction.setTitle({
-				tabId: tabId,
-				title: browser.i18n.getMessage('through_proxy')
-			});
+			browser.pageAction.setIcon({ tabId: tabId, path: 'icons/no-ip.svg' });
+			browser.pageAction.setTitle({ tabId: tabId, title: browser.i18n.getMessage('through_proxy') });
 		} else {
-			browser.pageAction.setTitle({
-				tabId: tabId,
-				title: tab.onResponseStarted.ip
-			});
+			browser.pageAction.setTitle({ tabId: tabId, title: tab.onResponseStarted.ip });
 			if (tab.onCommitted.transitionQualifiers.indexOf('forward_back') !== -1) {
-				browser.pageAction.setIcon({
-					tabId: tabId,
-					path: 'icons/ip.svg'
-				});
+				browser.pageAction.setIcon({ tabId: tabId, path: 'icons/ip.svg' });
 			}
 		}
 		delete tabs[tabId];
@@ -61,7 +34,6 @@ function updateIcon(details, event) {
 
 browser.webRequest.onResponseStarted.addListener(
 	(details) => {
-		if (details.frameId !== 0) return;
 		updateIcon(details, 'onResponseStarted');
 	},
 	{
@@ -89,8 +61,7 @@ browser.tabs.onRemoved.addListener((tabId) => {
 });
 
 browser.pageAction.onClicked.addListener((tab) => {
-	let gettingTitle = browser.pageAction.getTitle({
-		tabId: tab.id
+	browser.pageAction.getTitle({ tabId: tab.id }).then((title) => {
+		navigator.clipboard.writeText(title)
 	});
-	gettingTitle.then((title) => navigator.clipboard.writeText(title));
 });
